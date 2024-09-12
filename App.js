4bf5ea2498210bc1864d7884bc3930730c31b58c
taowu-neu <wu.tao1@northeastern.edu>
2024-09-12 11:34:08 -0700

    declare a const variable

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = 'Tao App';
+
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
