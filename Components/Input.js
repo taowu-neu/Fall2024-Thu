d9fec0ba9a0544c0ec03867e3db6f4651f2748e9
taowu-neu <wu.tao1@northeastern.edu>
2024-09-12 20:16:10 -0700

    Check whether the characters are greater than 3 or not

--- Components/Input.js
+++ Components/Input.js
@@ -2,7 +2,7 @@ import { TextInput, Text, StatusBar, StyleSheet, View } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 
-const Input = () => {
+const Input = (props) => {
   const [text, setText] = useState('');
   const [isFocused, setIsFocused] = useState(false);
 
@@ -27,8 +27,13 @@ const Input = () => {
         setText(changedText);
       }}/>
 
-{isFocused && text.length > 0 && <Text>Counting character: {text.length}</Text>}
+    {isFocused && text.length > 0 && <Text>Counting character: {text.length}</Text>}
 
+    {!isFocused && (
+        <Text>
+        {text.length < 3 ? 'Please type more than 3 characters' : 'Thank you'}
+        </Text>
+    )}
     </View>
   )
 }
