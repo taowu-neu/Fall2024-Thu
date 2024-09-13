16f4f85aa421488353efb7ba5911b75569960691
taowu-neu <wu.tao1@northeastern.edu>
2024-09-12 19:00:20 -0700

    Add the isFocused state to manage the focus status

--- Components/Input.js
+++ Components/Input.js
@@ -4,6 +4,14 @@ import { useState } from 'react';
 
 const Input = () => {
   const [text, setText] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  }
+  const handleBlur = () => {
+    setIsFocused(false);
+  }
 
   return (
     <View>
@@ -13,6 +21,8 @@ const Input = () => {
       style={{borderBottomColor: "purple", borderBottomWidth: 2}}
       value={text}
       autoFocus={props.focus}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
       onChangeText={function (changedText) {
         setText(changedText);
       }}/>
