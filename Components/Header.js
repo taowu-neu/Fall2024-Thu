4ac756d0a6a046c004419ea330d6c46d84d82155
taowu-neu <wu.tao1@northeastern.edu>
2024-09-19 14:26:03 -0700

    Modify the app and header component

--- Components/Header.js
+++ Components/Header.js
@@ -1,28 +1,26 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function Header(props) {
-
   return (
     <View style={styles.headerContainer}>
-      <Text style = {styles.headerStyle}>Welcome to {props.name}</Text>
-      {/* <Text>{props.children}</Text> */}
+      <Text style={styles.headerStyle}>Welcome to {props.name}</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    headerStyle: {
-      fontSize: 20,
-      borderColor: 'purple',
-      borderWidth: 2,
-      margin: 15,
-      padding: 10,
-      color: 'purple',
-    },
-    headerContainer: {
-        alignItems: 'center', 
-        justifyContent: 'center',
-        flex: 1,
-      },
-  })
+  headerStyle: {
+    fontSize: 20,
+    borderColor: 'purple',
+    borderWidth: 2,
+    margin: 15,
+    padding: 10,
+    color: 'purple',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
