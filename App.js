e3bd2781c099c7aba707ae10b90e85ba264ab0e2
taowu-neu <wu.tao1@northeastern.edu>
2024-09-26 12:03:57 -0700

    wrap the list of goals inside a ScrollView component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
 import Header from './Components/Header';
 import { useState } from 'react';
 import Input from './Components/Input';
@@ -30,13 +30,13 @@ export default function App() {
         <Button title='Add a goal' onPress={() => setModalVisible(true)} />
       </View>
       <Input focus={true} inputHandler={handleInputData} isModalVisible={modalVisible} cancelHandler={handleCancel} />
-      <View style={styles.bottomContainer}>
+      <ScrollView style={styles.bottomContainer} contentContainerStyle={styles.scrollViewContent}>
         {goals.map((item) => (
           <View key={item.id} style={styles.textContainer}>
             <Text style={styles.textStyle}>{item.text}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
   
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -68,9 +68,11 @@ const styles = StyleSheet.create({
   bottomContainer: {
     flex: 4,
     backgroundColor: '#FDD0E6',
-    alignItems: 'center',
-    rowGap: 10,
     width: '100%', 
     padding: 10,
-  }
+  },
+  scrollViewContent: {
+    alignItems: 'center',
+    rowGap: 10,
+  },
 });
