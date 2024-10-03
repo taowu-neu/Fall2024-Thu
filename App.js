7bf0369b9d00cc5dde9baeca92ba2149ead0b7cd
taowu-neu <wu.tao1@northeastern.edu>
2024-10-03 11:39:09 -0700

    createNativeStackNavigator finished

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
