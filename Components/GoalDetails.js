8e241066c74634c0e8d3908f13eb84e3c6e017e6
taowu-neu <wu.tao1@northeastern.edu>
2024-10-03 15:00:01 -0700

    use navigation to navigate directly to the GoalDetails

--- Components/GoalDetails.js
+++ Components/GoalDetails.js
@@ -1,25 +1,35 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function GoalDetails({ navigation, route }) {
-  console.log(route);
-  function moreDetailsHandler() {
-    navigation.push("Details");
+export default function GoalItem({ goalObj, deleteHandler, navigation }) {
+  function handleDelete() {
+    deleteHandler(goalObj.id);
+  }
+
+  function handlePress() {
+    navigation.navigate("Details", { goalData: goalObj });
   }
 
   return (
-    <View>
-      {route.params ? (
-        <Text>
-          This is details of a goal with text {route.params.goalData.text} and
-          id {route.params.goalData.id}
-        </Text>
-      ) : (
-        <Text>More details</Text>
-      )}
-      <Button title="More Details" onPress={moreDetailsHandler} />
+    <View style={styles.textContainer}>
+      <Text style={styles.text}>{goalObj.text}</Text>
+      <Button title="X" color="grey" onPress={handleDelete} />
+      <Button title="i" color="grey" onPress={handlePress} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    color: "purple",
+    padding: 5,
+    fontSize: 30,
+  },
+  textContainer: {
+    backgroundColor: "#aaa",
+    borderRadius: 5,
+    marginVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
