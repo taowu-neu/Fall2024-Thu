179781cf749b064c10211a50dd30549c0ff0df06
taowu-neu <wu.tao1@northeastern.edu>
2024-10-03 12:28:50 -0700

    Configuring Screen options dynamically using params

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Home';
 import GoalDetails from './Components/GoalDetails';
 
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -14,18 +13,25 @@ export default function App() {
           name="Home"
           component={Home}
           options={{
-            title: 'Home', 
-            headerStyle: { backgroundColor: '#dcd' }, 
-            headerTintColor: '#ffffff', 
+            title: 'Home',
+            headerStyle: { backgroundColor: 'purple' },
+            headerTintColor: '#ffffff',
           }}
         />
+        
         <Stack.Screen
           name="Details"
-          component={GoalDetails} 
+          component={GoalDetails}
+          options={({ route }) => ({
+            title: route.params.goalObj.text,
+            headerStyle: { backgroundColor: 'purple' },
+            headerTintColor: '#ffffff',
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
 
 
